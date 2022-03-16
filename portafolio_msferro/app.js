b53521bd9ca9d80529fd94ce564ecb125c115cb0
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-03-16 18:03:35 -0300

    Add files via upload

---
+++ portafolio_msferro/app.js
@@ -0,0 +1,62 @@
+//aqui comienza editar about
+//-----función que abre el div oculto-->
+function editar_about() {
+  document.getElementById("edit-about").style.display = "block";
+  let texto = document.getElementById("about_me").innerText;
+  console.log(texto);
+}
+function myFunction3(value) {
+  document.getElementById("about_me").innerText = value;
+}
+function logMessage2(message) {
+  console.log(message + "<br>");
+}
+//controla el enter
+let textArea = document.getElementById("edit-about");
+textArea.addEventListener("keyup", (e) => {
+  logMessage(`key "${e.key}"  released [event:keyup]`);
+  if (e.key == "Enter") {
+    document.getElementById("edit-about").style.display = "none";
+  }
+});
+
+//aquí comienza editar footer
+function cambiar_parrafo() {
+  document.getElementById("edit-footer").style.display = "block";
+  let texto = document.getElementById("text-footer").innerText;
+  console.log(texto);
+}
+
+function myFunction2(value) {
+  document.getElementById("text-footer").innerText = value;
+}
+function logMessage(message) {
+  console.log(message + "<br>");
+}
+
+//controla el enter
+let textarea = document.getElementById("edit-footer");
+textarea.addEventListener("keyup", (e) => {
+  logMessage(`key "${e.key}"  released [event:keyup]`);
+  if (e.key == "Enter") {
+    document.getElementById("edit-footer").style.display = "none";
+  }
+});
+
+//cargar el contenido de archivo externo y lo muestra para eso en el html agregar un input de tipo file
+function showFile(input) {
+  let file = input.files[0];
+  alert(`File name: ${file.name}`);
+  alert(`Last modified: ${file.lastModified}`);
+
+  let reader = new FileReader();
+  reader.readAsText(file);
+  reader.onload = function () {
+    console.log(reader.result);
+    document.getElementById("text-footer").innerText = reader.result;
+  };
+  reader.onerror = function () {
+    console.log(reader.error);
+  };
+}
+//aquí termina editar footer
